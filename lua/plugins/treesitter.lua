366e451edb1978f481a582bb5b64bf2e93509ce7
songshaobo <shaobo.song@yudt.io>
2025-02-12 09:46:44 +0800

    Update nvim-treesitter configuration
    Signed-off-by: songshaobo <shaobo.song@yudt.io>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,9 @@ return {
     config = function()
         require("nvim-treesitter.configs").setup({
             ensure_installed = { "lua", "c", "cpp" },
+            sync_install = false,
+            auto_install = false,
+            ignore_install = {},
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
@@ -13,6 +16,16 @@ return {
             indent = {
                 enable = true,
             },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<ESC>v",
+                    node_incremental = "<ESC>v",
+                    node_decremental = "<ESC>V",
+                    -- scope_incremental = "",
+                },
+            },
+            modules = {},
         })
     end,
 }
