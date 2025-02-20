924037b953f1fa55d1d1f2b1db34c6fdca37be67
songshaobo <shaobo.song@yudt.io>
2025-02-20 13:37:57 +0800

    Remove nvim-tree plugin and enable netrw builtin-plugin
    Signed-off-by: songshaobo <shaobo.song@yudt.io>

--- lua/config/variable.lua
+++ lua/config/variable.lua
@@ -3,8 +3,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 vim.g.vim_copy_clipboard = '~/.vim-copy-clipboard'
 
