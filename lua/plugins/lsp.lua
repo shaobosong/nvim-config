4578eea922c3f61472d0ffa2fb5286edcec38ece
songshaobo <shaobo.song@yudt.io>
2025-02-20 13:28:58 +0800

    Add configuration for mason-nvim-lspconfig
    Signed-off-by: songshaobo <shaobo.song@yudt.io>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,7 +8,11 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
-            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "clangd" },
+            automatic_installation = false,
+            handlers = nil,
+        })
         end,
     },
     {
