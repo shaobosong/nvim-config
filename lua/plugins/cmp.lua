9ba7807d8b211bf67362e3345cc58664b51c2d7b
songshaobo <shaobo.song@yudt.io>
2025-02-23 19:08:35 +0800

    Add snippet engine in cmp.lua
    Signed-off-by: songshaobo <shaobo.song@yudt.io>

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,6 +6,8 @@ return {
         { 'hrsh7th/cmp-buffer', lazy = true },
         { 'hrsh7th/cmp-path', lazy = true },
         { 'hrsh7th/cmp-cmdline', lazy = true },
+        -- { 'L3MON4D3/LuaSnip', lazy = true },
+        -- { 'saadparwaiz1/cmp_luasnip', lazy = true },
         { 'dmitmel/cmp-cmdline-history', lazy = true },
     },
     opts = function(_, opts)
@@ -15,6 +17,16 @@ return {
     config = function()
         local cmp = require'cmp'
         cmp.setup{
+            snippet = {
+                expand = function(args)
+                    -- For native neovim snippets (Neovim v0.10+)
+                    vim.snippet.expand(args.body)
+                    -- For `luasnip` users.
+                    -- require('luasnip').lsp_expand(args.body)
+                    -- For `snippy` users.
+                    -- require('snippy').expand_snippet(args.body)
+                end,
+            },
             window = {
                 completion = {
                     border = 'single',
@@ -29,6 +41,8 @@ return {
             },
             preselect = cmp.PreselectMode.Item,
             mapping = cmp.mapping.preset.insert({
+                ['<S-Up>'] = cmp.mapping.scroll_docs(-4),
+                ['<S-Down>'] = cmp.mapping.scroll_docs(4),
                 ['<ESC>j'] = {
                     i = function()
                         if cmp.visible() then
@@ -55,15 +69,39 @@ return {
                         vim.api.nvim_input('<Esc><Esc>')
                     end
                 },
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    -- For native neovim snippets (Neovim v0.10+)
+                    if vim.snippet.active({ direction = 1 }) then
+                        vim.snippet.jump(1)
+                    -- For `luasnip` users
+                    -- if require('luasnip').expand_or_jumpable() then
+                    --     require('luasnip').expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    -- For native neovim snippets (Neovim v0.10+)
+                    if vim.snippet.active({ direction = -1 }) then
+                        vim.snippet.jump(-1)
+                    -- For `luasnip` users
+                    -- if require('luasnip').jumpable(-1) then
+                    --     require('luasnip').expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
                 -- [''] = cmp.mapping.complete(),
                 -- [''] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior, count = 1 }),
                 -- [''] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior, count = 1 }),
-                -- [''] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Insert }),
+                ['<CR>'] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Insert }),
 
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'nvim_lua' },
+                -- { name = 'luasnip' }, -- For luasnip users.
+                -- { name = 'snippy' }, -- For snippy users.
                 -- { name = 'buffer' },
                 -- { name = 'path' },
                 -- { name = 'cmdline' },
@@ -79,7 +117,25 @@ return {
                     end
                 end,
             },
+            ['<M-j>'] = {
+                c = function()
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        cmp.complete()
+                    end
+                end,
+            },
             ['<S-Tab>'] = {
+                c = function()
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    else
+                        cmp.complete()
+                    end
+                end,
+            },
+            ['<M-k>'] = {
                 c = function()
                     if cmp.visible() then
                         cmp.select_prev_item()
