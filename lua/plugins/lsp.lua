80dd6632809cebee05b09348be4e30398d33c3d3
Shaobo Song <shnusongshaobo@gmail.com>
2025-04-21 09:26:47 +0800

    Update lsp configuration style and remove ensured installed entries
    Signed-off-by: Shaobo Song <shnusongshaobo@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd" },
+                ensure_installed = {},
                 automatic_installation = false,
                 handlers = nil,
             })
@@ -27,64 +27,77 @@ return {
             local lspconfig = require("lspconfig")
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities.textDocument.completion.completionItem.snippetSupport = true
-            -- C-family
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-                root_dir = function(fname)
-                    return require("lspconfig.util").root_pattern(
-                        ".git",
-                        "Makefile",
-                        "configure.ac",
-                        "configure.in",
-                        "config.h.in",
-                        "meson.build",
-                        "meson_options.txt",
-                        "build.ninja",
-                        "compile_commands.json",
-                        "compile_flags.txt"
-                    )(fname) or vim.fs.dirname(
-                        vim.fs.find('.git', { path = fname, upward = true })[1]
-                    )
-                end,
-                cmd = {
-                    'clangd',
-                    '--compile-commands-dir=build',
-                    '--background-index',
-                    '--completion-style=detailed',
-                    '--header-insertion=iwyu',
-                    -- '--clang-tidy',
-                    -- '--inlay-hints',
-                    -- "--function-arg-placeholders",
-                    -- "--fallback-style=llvm",
+            local servers = {
+                {
+                    name = "clangd",
+                    bin = "clangd",
+                    config = {
+                        capabilities = capabilities,
+                        root_dir = function(fname)
+                            return require("lspconfig.util").root_pattern(
+                                ".git",
+                                "Makefile",
+                                "configure.ac",
+                                "configure.in",
+                                "config.h.in",
+                                "meson.build",
+                                "meson_options.txt",
+                                "build.ninja",
+                                "compile_commands.json",
+                                "compile_flags.txt"
+                            )(fname) or vim.fs.dirname(
+                                vim.fs.find('.git', { path = fname, upward = true })[1]
+                            )
+                        end,
+                        cmd = {
+                            'clangd',
+                            '--compile-commands-dir=build',
+                            '--background-index',
+                            '--completion-style=detailed',
+                            '--header-insertion=iwyu',
+                            -- '--clang-tidy',
+                            -- '--inlay-hints',
+                            -- "--function-arg-placeholders",
+                            -- "--fallback-style=llvm",
+                        },
+                        init_options = {
+                            usePlaceholders = true,
+                            completeUnimported = true,
+                            clangdFileStatus = true,
+                        },
+                        single_file_support = true,
+                    },
                 },
-                init_options = {
-                    usePlaceholders = true,
-                    completeUnimported = true,
-                    clangdFileStatus = true,
-                },
-                single_file_support = true,
-            })
-            -- Lua
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                            globals = { 'vim' },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false,
-                        },
-                        telemetry = {
-                            enable = false,
+                {
+                    name = "lua_ls",
+                    bin = "lua-language-server",
+                    config = {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT',
+                                },
+                                diagnostics = {
+                                    globals = { 'vim' },
+                                },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false,
+                                },
+                                telemetry = {
+                                    enable = false,
+                                },
+                            },
                         },
                     },
                 },
-            })
+            }
+            for _, server in ipairs(servers) do
+                if vim.fn.exepath(server.bin) ~= "" then
+                    lspconfig[server.name].setup(server.config)
+                end
+            end
         end,
     },
 }
